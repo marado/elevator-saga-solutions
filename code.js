d8153339ba8bd0cc751d9e8670b401534bcd4b24
Marcos Marado <mindboosternoori@gmail.com>
2015-01-23 21:29:16 +0000

    Passing level 1
    Warning:
    * Only one elevator is working
    * The elevator is stupid

--- code.js
+++ code.js
@@ -1,12 +1,30 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-        elevator.on("idle", function() {
-            // The elevator is idle, so let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-        });
+		// helper functions
+		sendElevatorTo = function(floor) {
+			// for now, send the first elevator
+			elevators[0].goToFloor(floor);
+		}
+
+		// elevator events
+		for (var e = 0; e < elevators.length; e++) {
+        	var elevator = elevators[e];
+        	elevator.on("idle", function() {
+	       	});
+			elevator.on("floor_button_pressed", function(floor) {
+        	    elevator.goToFloor(floor);
+			});
+		}
+
+		// floor events
+		for (var f = 0; f < floors.length; f++) {
+			var floor = floors[f];
+			floor.on("up_button_pressed down_button_pressed", function() {
+				sendElevatorTo(this.floorNum());
+			});
+		}
     },
+
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
