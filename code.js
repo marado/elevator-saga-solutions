e13486a80d52fde9bb505b744482c15e7074b93a
Marcos Marado <mindboosternoori@gmail.com>
2015-01-23 22:00:31 +0000

    Crashing the game
    I wrote this on my terminal, pasted this onto the game and applying it...
    it crashes, both on Firefox and on Chrome.
    
    I probably have a bug on my code that is causing the crash, but the game
    should behave better anyway, so I'm commiting this to open a bug report.

--- code.js
+++ code.js
@@ -1,9 +1,31 @@
 {
     init: function(elevators, floors) {
 		// helper functions
-		sendElevatorTo = function(floor) {
-			// for now, send the first elevator
-			elevators[0].goToFloor(floor);
+		sendOneElevatorTo = function(floor) {
+			// TODO: for now, always send the first elevator
+			sendElevatorTo(0,floor);
+		}
+
+		sendElevatorTo = function(elevator, floor) {
+			// we might want to reschedule stuff here
+			// for now, let's try a weird trick: stop everywhere on the way!
+			// TODO: go from the last queued destination to floor, instead of "here to floor"
+			var distance = elevators[elevator].currentFloor() - floor;
+			if (distance > 1) {
+				// going down
+				for (var stop = elevators[elevator].currentFloor() - 1; stop >= floor; stop--) {
+					elevators[elevator].goToFloor(stop);
+				}
+			} else if (distance < 1) {
+				// going up
+				for (var stop = elevators[elevator].currentFloor() + 1; stop >= floor; stop++) {
+					elevators[elevator].goToFloor(stop);
+				}
+
+			} else {
+				// it's right here!
+				elevators[elevator].goToFloor(floor);
+			}
 		}
 
 		// elevator events
@@ -12,7 +34,16 @@
         	elevator.on("idle", function() {
 	       	});
 			elevator.on("floor_button_pressed", function(floor) {
-        	    elevator.goToFloor(floor);
+        	    this.goToFloor(floor);
+			});
+			elevator.on("stopped_at_floor", function(floorNum) {
+				// let's rethink our schedule
+				var queue = this.destinationQueue;
+				if (queue.length > 0) {
+					// TODO: this is where we do voodoo:
+					// this.stop(); // yeah, I'm drastic
+					// this.currentFloor();
+				}
 			});
 		}
 
@@ -20,7 +51,7 @@
 		for (var f = 0; f < floors.length; f++) {
 			var floor = floors[f];
 			floor.on("up_button_pressed down_button_pressed", function() {
-				sendElevatorTo(this.floorNum());
+				sendOneElevatorTo(this.floorNum());
 			});
 		}
     },
