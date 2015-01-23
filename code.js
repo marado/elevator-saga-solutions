d13100571696f3c51cfd56e9c4f918f220d008f3
Marcos Marado <mindboosternoori@gmail.com>
2015-01-23 23:22:24 +0000

    Passing level 2
    The elevator is still quite stupid. But a little less so.

--- code.js
+++ code.js
@@ -3,29 +3,14 @@
 		// helper functions
 		sendOneElevatorTo = function(floor) {
 			// TODO: for now, always send the first elevator
-			sendElevatorTo(0,floor);
+			sendElevatorTo(elevators[0],floor);
 		}
 
 		sendElevatorTo = function(elevator, floor) {
-			// we might want to reschedule stuff here
-			// for now, let's try a weird trick: stop everywhere on the way!
-			// TODO: go from the last queued destination to floor, instead of "here to floor"
-			var distance = elevators[elevator].currentFloor() - floor;
-			if (distance > 1) {
-				// going down
-				for (var stop = elevators[elevator].currentFloor() - 1; stop >= floor; stop--) {
-					elevators[elevator].goToFloor(stop);
-				}
-			} else if (distance < 1) {
-				// going up
-				for (var stop = elevators[elevator].currentFloor() + 1; stop >= floor; stop++) {
-					elevators[elevator].goToFloor(stop);
-				}
+			// if this elevator is already scheduled there, don't add to the queue
+			if (elevator.destinationQueue.indexOf(floor) != -1) return;
 
-			} else {
-				// it's right here!
-				elevators[elevator].goToFloor(floor);
-			}
+				elevator.goToFloor(floor);
 		}
 
 		// elevator events
@@ -34,7 +19,7 @@
         	elevator.on("idle", function() {
 	       	});
 			elevator.on("floor_button_pressed", function(floor) {
-        	    this.goToFloor(floor);
+				sendElevatorTo(this,floor);
 			});
 			elevator.on("stopped_at_floor", function(floorNum) {
 				// let's rethink our schedule
